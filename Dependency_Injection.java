9b40379ceeed015dd7f164aea0cc9bfa60e922b3
Nazmul Khan Liton <41168894+nazmulkhanliton@users.noreply.github.com>
2021-04-23 23:51:58 +0600

    1

---
+++ Dependency_Injection.java
@@ -0,0 +1,7 @@
+public class Dependency_Injection {
+
+    Test_Dependency_Injection ob;
+    public void doWork(){
+        System.out.println("Dependency_Injection");
+    }
+}
---
+++ Test_Dependency_Injection.java
@@ -0,0 +1,5 @@
+public class Test_Dependency_Injection {
+    public void doWork(){
+        System.out.println("Test_Dependency_Injection");
+    }
+}
